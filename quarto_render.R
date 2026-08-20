4520fd84d86250c24fdbb433e5f2a0b461b70728
Francis Lessard <frles121@ulaval.ca>
2026-08-20 11:29:10 -0400

    render

--- quarto_render.R
+++ quarto_render.R
@@ -1,3 +1,8 @@
 library(quarto)
 
 quarto_render()
+
+quarto_render("./index.qmd")
+quarto_render("./data_and_method.qmd")
+quarto_render("./project_outputs.qmd")
+quarto_render("./calibration_grm.qmd")
